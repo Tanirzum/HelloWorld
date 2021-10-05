6504ae522ec032708289ff3233daba9f64d8ad04
Tanirbergen <87113165+Tanirzum@users.noreply.github.com>
2021-10-05 13:05:02 +0600

    finallcommit

---
+++ src/DrumKit.java
@@ -0,0 +1,12 @@
+public class DrumKit {
+    boolean topHat = true;
+    boolean snare = true;
+
+    public void playSnare() {
+        System.out.println("Hello");
+    }
+
+    public void playTopHat() {
+        System.out.println("World");
+    }
+}
